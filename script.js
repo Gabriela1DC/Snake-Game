68f4793d4587e981948cab1a5cff42e3fcf74dca
Gabriela Costa <davidbia6@gmail.com>
2021-07-09 18:38:26 -0300

    Terceiro Commit

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ snake[0]={
 }
 
 let direction="right";
+let food = {
+    x: Math.floor(Mat.random() * 15 + 1) * box,
+    y: Math.floor(Mat.random() * 15 + 1) * box
+}
 
 function createBG(){
     
@@ -18,15 +22,42 @@ function createBG(){
 
 function createSnake() {
     for(i=0; i < snake.length; i++){
-        context.fillStyle='green'
+        context.fillStyle='green';
         context.fillReact(snake[i].x, snake[i].y, box, box);
     }
 }
 
+function drawFood() {
+    context.fillStyle='yellow';
+    context.fillReact(food.x, food.y, box, box);
+}
+
+document.addEventListener('keydown', update);
+
+function update(event) {
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
 function startGame(){
+
+    for (i=1; i < snake.length; i++){
+        if(snake[0].x==snake[i].x && snake[0].y==snake[i].y){
+            clearInterval(jogo);
+            alert('Fim de Jogo!!!');
+        }
+    }
+
+    if(snake[0].x > 15*box && direction == 'right') snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16*box;
+    if(snake[0].y > 15*box && direction == 'down') snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16*box;
 
     createBG();
     createSnake();
+    drawFood();
 
     let snake_X = snake[0].x;
     let snake_Y = snake[0].y;
@@ -36,7 +67,12 @@ function startGame(){
     if(direction=='up') snake_Y += box;
     if(direction=='down') snake_Y -= box;
 
-    snake.pop();
+    if(snake_X != food.x || food.y) {
+        snake.pop();
+    }
+    else{food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Mat.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snake_X,
